6f7e367e9b8d19e8f2428b3cddd813aad32d381e
Jakub Krzyśków <jakub.krzyskow@gmail.com>
2017-05-29 12:02:45 +0200

    Fixed name, type, cookies and file input stream

--- android/src/main/java/com/yoloci/fileupload/FileUploadModule.java
+++ android/src/main/java/com/yoloci/fileupload/FileUploadModule.java
@@ -1,6 +1,8 @@
 package com.yoloci.fileupload;
 
 import android.os.Bundle;
+import android.net.Uri;
+import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReadableArray;
@@ -10,20 +12,29 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.modules.network.ForwardingCookieHandler;
 
 import java.io.DataInputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URI;
 import java.io.DataOutputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.CookieManager;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import com.facebook.react.bridge.WritableMap;
 import java.io.FileInputStream;
+import java.io.InputStream;
 
 import org.json.JSONObject;
 
 public class FileUploadModule extends ReactContextBaseJavaModule {
+
+    private ForwardingCookieHandler cookieHandler;
 
     @Override
     public String getName() {
@@ -32,6 +43,7 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
 
     public FileUploadModule(ReactApplicationContext reactContext) {
         super(reactContext);
+        this.cookieHandler = new ForwardingCookieHandler(reactContext);
     }
 
     @ReactMethod
@@ -58,17 +70,17 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
         DataOutputStream outputStream = null;
         DataInputStream inputStream = null;
         URL connectURL = null;
-        FileInputStream fileInputStream = null;
+        URI connectURI = null;
+        //FileInputStream fileInputStream = null;
+        InputStream fileInputStream = null;
 
         int bytesRead, bytesAvailable, bufferSize;
         byte[] buffer;
         int maxBufferSize = 1*1024*1024;
 
         try {
-
             connectURL = new URL(uploadUrl);
-
-
+            connectURI = new URI(uploadUrl);
             connection = (HttpURLConnection) connectURL.openConnection();
 
             // Allow Inputs &amp; Outputs.
@@ -78,17 +90,21 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
 
             connection.setRequestMethod(method);
 
+            Map<String, List<String>> cookieMap = this.cookieHandler.get(connectURI,  new HashMap());
+            List<String> cookieList = cookieMap.get("Cookie");
+            if (cookieList != null) {
+                connection.setRequestProperty("Cookie", cookieList.get(0));
+            }
+
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
+
             // set headers
             ReadableMapKeySetIterator iterator = headers.keySetIterator();
             while (iterator.hasNextKey()) {
                 String key = iterator.nextKey();
                 connection.setRequestProperty(key, headers.getString(key));
             }
-
-
-
-            connection.setRequestProperty("Connection", "Keep-Alive");
-            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);
 
             outputStream = new DataOutputStream( connection.getOutputStream() );
 
@@ -106,14 +122,23 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
 
             for (int i = 0; i < files.size(); i++) {
 
+                Log.i(this.getName(), "Load file");
+
                 ReadableMap file = files.getMap(i);
                 String filename = file.getString("filename");
                 String filepath = file.getString("filepath");
-                filepath = filepath.replace("file://", "");
-                fileInputStream = new FileInputStream(filepath);
+                String filetype = file.getString("filetype");
+                String fileId = file.getString("name");
+                //filepath = filepath.replace("file://", "");
+                //fileInputStream = new FileInputStream(filepath);
+                fileInputStream = getReactApplicationContext().getContentResolver().openInputStream(Uri.parse(filepath));
+
+                Log.i(this.getName(), "Loaded file");
 
                 outputStream.writeBytes(twoHyphens + boundary + lineEnd);
-                outputStream.writeBytes("Content-Disposition: form-data; name=\"image\";filename=\"" + filename + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + fileId + "\";filename=\"" + filename + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Type: " + filetype + lineEnd);
+
                 outputStream.writeBytes(lineEnd);
 
                 bytesAvailable = fileInputStream.available();
@@ -130,21 +155,35 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
                     bytesRead = fileInputStream.read(buffer, 0, bufferSize);
                 }
 
+                Log.i(this.getName(), "File read ready");
+
                 outputStream.writeBytes(lineEnd);
             }
+
+            Log.i(this.getName(), "Before out");
 
             outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
 
             // Responses from the server (code and message)
 
+            Log.i(this.getName(), "Before connection");
+
             int serverResponseCode = connection.getResponseCode();
             String serverResponseMessage = connection.getResponseMessage();
-            if (serverResponseCode != 200) {
-                fileInputStream.close();
+            Log.i(this.getName(), "Got response");
+
+            if (serverResponseCode != 200 && serverResponseCode != 201) {
+                Log.i(this.getName(), "Not 200");
+
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
                 outputStream.flush();
                 outputStream.close();
                 callback.invoke("Error happened: " + serverResponseMessage, null);
             } else {
+                Log.i(this.getName(), "Ok");
+
                 BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
                 StringBuilder sb = new StringBuilder();
                 String line;
@@ -161,15 +200,19 @@ public class FileUploadModule extends ReactContextBaseJavaModule {
                 Bundle bundle = bjc.convertToBundle(mainObject);
                 WritableMap map = Arguments.fromBundle(bundle);
 
-                fileInputStream.close();
+                if (fileInputStream != null) {
+                    fileInputStream.close();
+                }
+                
                 outputStream.flush();
                 outputStream.close();
                 callback.invoke(null, map);
             }
 
-
+            Log.i(this.getName(), "End");
 
         } catch(Exception ex) {
+            Log.i(this.getName(), "Exception");
             callback.invoke("Error happened: " + ex.getMessage(), null);
         }
     }
